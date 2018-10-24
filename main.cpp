3ea239c391e49478b6182d79bceaf3052c271e39
Mariana Acosta Zermeno <mariana-az@hotmail.com>
2018-10-23 20:48:06 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-// Author:       Mariana Acosta
-// Assignment:       project 7
-// File:       Graph
-// Instructor:       Reza Sanati
-// Class:       CSIS 2420  Section: 001
-// Date Written:   June 20, 2018
-// Description: get data from file build a graph of the possible flights, then find the shortest path from place to place based on the cost 
-
 #include "graph.h"
 #include <fstream>
 #include <iostream>
@@ -19,12 +11,6 @@ using namespace std;
 
 const int INFINITY = INT_MAX;
 
-// Function Name:    read_data
-// Purpose:       read the input data and store it in different variables (src, dest, mile, cost)
-// Parameters:      ifstream &input, graph &g
-// Returns:       none void
-// Pre-conditions:  while there's data pass it, if it equals to infinity add vertex 
-// Post-conditions:none
 void read_data(ifstream &input, graph &g) {
     string src;
     string dest;
@@ -69,8 +55,7 @@ int main() {
     cout << "\n\t\t\t\t Connecting Flight Information";
     cout << "\nSource Dest\tCost  Mileage   Src  Dest \tCost  \tMileage" << endl;
     cout << "-----------------------------------------------------------------" << endl;
-    
-    //    flights.print();
+
     flights.shortestPath(sourcePlace); //call the shortest path function
     
 }
